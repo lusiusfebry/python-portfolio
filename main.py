ae2b990d3fe14348d5d3bf6d706402b9a73bef03
febrianus.maro <lusiusfebry@gmail.com>
2023-02-08 16:31:15 +0800

    day 22:add description ,image and links

--- main.py
+++ main.py
@@ -24,12 +24,19 @@ st.write(content_app)
 
 df = pd.read_csv("data.csv",sep=";")
 
-col3,col4 = st.columns(2)
+col3,empty_col,col4 = st.columns([1.4,0.5,1.5])
 with col3:
     for index,row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        # st.write("[source code](https://pythonhow.com")
+        st.write(f"[Source Code]({row['url']}")
 
 with col4:
     for index,row in df[10:].iterrows():
         st.header(row["title"])
-
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write("[source code](https://pythonhow.com")
+        st.write(f"[Source Code]({row['url']}")
